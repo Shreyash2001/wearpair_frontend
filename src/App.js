afd273f04a3b0940c58eb45394633a1456394a0a
Shreyash2001 <shreyashkatare21@gmail.com>
2025-01-25 00:34:11 +0530

    feat: added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
 import HomePage from "./Screens/HomePage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import OutfitDetailsPage from "./Screens/OutfitDetailsPage";
 
 function App() {
   return (
-    <div className="app">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/outfit/:id" element={<OutfitDetailsPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Screens/OutfitDetailsPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function OutfitDetailsPage() {
+  return <div>OutfitDetailsPage</div>;
+}
+
+export default OutfitDetailsPage;
